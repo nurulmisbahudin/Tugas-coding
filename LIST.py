adc81b73ad1ac3961ff655f5c21528bda2388f79
nurulmisbahudin <37294673+nurulmisbahudin@users.noreply.github.com>
2018-03-21 21:52:26 +0700

    Add files via upload

--- LIST.py
+++ LIST.py
@@ -7,15 +7,14 @@ while(jawab == 'ya'):
     nuts = input("NILAI UTS :")
     nuas = input("NILAI UAS :")
     nakhir = int(nt*30/100)+(nuts*35/100)+(nuas*35/100)
-    nakhir = int(nt*30/100)+(nuts*35/100)+(nuas*35/100)
     jawab = raw_input("Tambah data (ya/tidak) :")
     if jawab == 'tidak':
         while(jawab == 'tidak'):
-            print "____________________________________________________________"
+            print"____________________________________________________________"
             print "| NO | NAMA|  NIM  | N.TUGAS | N.UTS | N.UAS | N.AKHIR |"
             print "------------------------------------------------------------"
-            print "|",no," | ",nama,"| ",nim,"|",nt,"|",nuts," |",nuas," |",nakhir,"|"
-              
-
-
-    
+            print"|",no," | ",nama,"| ",nim,"|",nt,"|",nuts," |",nuas," |",nakhir,"|"
+            break    
+        
+        
+            
